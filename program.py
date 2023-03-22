fd7a7772a09f51696ea9adc3eecac3541eb66662
Tim Schmid <tim.a.schmid@gmail.com>
2023-03-22 21:09:30 +1100

    added commenting

--- program.py
+++ program.py
@@ -1,50 +1,26 @@
 #SID: 500687806
-
-'''
-UTILITY(state):
-	if red is winner:
-		return 10000
-	if yellow is winner
-		return -10000
-
-EVALUATION(state):
-	return SCORE(state, red player) – SCORE(state, yellow player)
-
-SCORE(state, player):
-	return number of tokens of player’s colour +
-		10 * NUM_IN_A_ROW(2, state, player) +
-		100 * NUM_IN_A_ROW(3, state, player) +
-		1000 * NUM_IN_A_ROW(4 or more, state, player)
-
-NUM_IN_A_ROW(count, state, player):
-	returns the number of times that <state> contains a <count>-in-a-row
-for the given <player>
-'''
-
-
-'''
-https://towardsdatascience.com/creating-the-perfect-connect-four-ai-bot-c165115557b0
-Bitboard Representation of state
-
-|6 13 20 27 34 41 48| <- Auxilary Row
-+===================+
-|5 12 19 26 33 40 47|
-|4 11 18 25 32 39 46|
-|3 10 17 24 31 38 45|
-|2  9 16 23 30 37 44|
-|1  8 15 22 29 36 43|
-|0  7 14 21 28 35 42|
-+===================+
-
-'''
-
-HEIGHT = 6
-AUX_HEIGHT = HEIGHT + 1
-WIDTH = 7
-
 class Board:
+    '''
+    Bitboard Representation of state.
+    Board is represented as a bitstring with 0'th position being LSB
+
+    |6 13 20 27 34 41 48| <- Auxilary Row
+    +===================+
+    |5 12 19 26 33 40 47|
+    |4 11 18 25 32 39 46|
+    |3 10 17 24 31 38 45|
+    |2  9 16 23 30 37 44|
+    |1  8 15 22 29 36 43|
+    |0  7 14 21 28 35 42|
+    +===================+
+    '''
+
     def __init__(self):
-        self.col_height = [0]*WIDTH
+
+        self.height = 6
+        self.width = 7
+
+        self.col_height = [0]*self.width
 
         self.cur_player_state = b''
         self.cur_player_colour = ''
@@ -62,15 +38,16 @@ def convert_state_to_player_pos(turn, contents):
     cur_player_state, board_state = '', ''
 
     rows = contents.split(',')
-    for i in range (WIDTH-1,-1,-1):
+    for i in range (board.width-1,-1,-1):
 
         #add auxilary row for bitwise manipulation
         cur_player_state += '0'
         board_state += '0'
 
-        for j in range(HEIGHT-1,-1,-1):
+        #setup states for the overall board and current player (us)
+        for j in range(board.height-1,-1,-1):
             if rows[j][i] == '.':
-                board.col_height[i] = i*(AUX_HEIGHT)+j
+                board.col_height[i] = i*(board.height+1)+j
                 cur_player_state += '0'
                 board_state += '0'
             elif rows[j][i] == turn:
@@ -83,11 +60,14 @@ def convert_state_to_player_pos(turn, contents):
 
     board.cur_player_state = int(cur_player_state,2)
     board.board_state = int(board_state,2)
+
+    #calcuate other player's state by XOR the main board with us
     board.other_player_state = board.cur_player_state ^ board.board_state
 
     board.moves_made = board.board_state.bit_count()
     board.cur_player_colour = turn
 
+    #setup player colours (important for printing)
     if turn == 'r':
         board.other_player_colour = 'y'
     else:
@@ -117,14 +97,21 @@ def evaluation(board):
 
 
 def score(pos):
+
+    #Note: we subtract double counts when counting how many 2/3 in a rows there are.
+    #      eg. in every 4 in a row there is 2 x three in a rows.
+ 
     four_in_row = num_in_row(4,pos)
-    three_in_row = num_in_row(3,pos) - 2*four_in_row
+    three_in_row = num_in_row(3,pos) - 2*four_in_row 
     two_in_row = num_in_row(2,pos) - 3*four_in_row - 2*three_in_row
     return pos.bit_count() + 10*two_in_row + 100*three_in_row + 1000*four_in_row
 
 def num_in_row(count, pos):
     total = 0
     directions = {VERTICAL,RIGHT_DIAG,LEFT_DIAG,HORIZONTAL}
+    
+    #Some bitshifting magic to found how many in a row we have.
+    #See: https://towardsdatascience.com/creating-the-perfect-connect-four-ai-bot-c165115557b0
     
     for dir in directions:
         m = pos
@@ -137,10 +124,10 @@ def num_in_row(count, pos):
 def print_board(board):
     print("Move " + str(board.moves_made))
     print('+===============+')
-    for i in range(HEIGHT,-1,-1):
+    for i in range(board.height,-1,-1):
         print('| ', end="")
-        for j in range(0,WIDTH):
-            cur = (i + j*(AUX_HEIGHT))
+        for j in range(0,board.width):
+            cur = (i + j*(board.height+1))
 
             if (board.cur_player_state & (1 << cur)):
                 print(board.cur_player_colour + ' ', end='')
@@ -154,11 +141,6 @@ def print_board(board):
     print('+===============+')
 
 def connect_four_mm(contents, turn, max_depth):
-
-
-
-
-    
 
     board = convert_state_to_player_pos(turn[0],contents)
     print_board(board)
