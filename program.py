6195038773bd6f9a143925cc9d70e768886e1b5e
Tim Schmid <tim.a.schmid@gmail.com>
2023-03-23 15:31:08 +1100

    minmax basic implementaiton

--- program.py
+++ program.py
@@ -1,4 +1,12 @@
 #SID: 500687806
+
+VERTICAL = 1
+RIGHT_DIAG = 6
+LEFT_DIAG = 8
+HORIZONTAL = 7
+
+RED = 0
+YEL = 1
 class Board:
     '''
     Bitboard Representation of state.
@@ -25,92 +33,99 @@ class Board:
         self.width = 7
 
         self.col_height = [0]*self.width
-
-        self.cur_player_state = b''
-        self.cur_player_colour = ''
-
-        self.other_player_state = b''
-        self.other_player_colour = ''
-
-        self.board_state = b''
         self.moves_made = 0
+
+        self.cur_player = 0
+        self.our_player = 0
+
+        self.player_states = [b'',b'']
+        self.board_state = b''
+        self.moves = []
 
 def convert_state_to_player_pos(turn, contents):
 
     board = Board()
     
-    cur_player_state, board_state = '', ''
+    red_state, yel_state = '', '',
 
     rows = contents.split(',')
     for i in range (board.width-1,-1,-1):
 
         #add auxilary row for bitwise manipulation
-        cur_player_state += '0'
-        board_state += '0'
+        red_state += '0'
+        yel_state += '0'
 
         #setup states for the overall board and current player (us)
         for j in range(board.height-1,-1,-1):
             if rows[j][i] == '.':
                 board.col_height[i] = i*(board.height+1)+j
-                cur_player_state += '0'
-                board_state += '0'
-            elif rows[j][i] == turn:
-                cur_player_state += '1'
-                board_state += '1'
+                red_state += '0'
+                yel_state += '0'
+            elif rows[j][i] == 'r':
+                red_state += '1'
+                yel_state += '0'
+            elif rows[j][i] == 'y':
+                red_state += '0'
+                yel_state += '1'
+                
+    board.player_states[RED] = int(red_state,2)
+    board.player_states[YEL] = int(yel_state,2)
 
-            else:
-                cur_player_state += '0'
-                board_state += '1'
+    #calcuate board state by bitwise OR with players
+    board.board_state = board.player_states[RED] | board.player_states[YEL]
 
-    board.cur_player_state = int(cur_player_state,2)
-    board.board_state = int(board_state,2)
+    board.moves_made = bin(board.board_state).count("1")
 
-    #calcuate other player's state by XOR the main board with us
-    board.other_player_state = board.cur_player_state ^ board.board_state
-
-    board.moves_made = board.board_state.bit_count()
-    board.cur_player_colour = turn
-
-    #setup player colours (important for printing)
     if turn == 'r':
-        board.other_player_colour = 'y'
+        board.our_player = RED
     else:
-        board.other_player_colour = 'r'     
+        board.our_player = YEL
+
+    board.cur_player = board.our_player
 
     return board
 
-VERTICAL = 1
-RIGHT_DIAG = 6
-LEFT_DIAG = 8
-HORIZONTAL = 7
 
 def make_move(board, col):
-    board.cur_player_state = board.cur_player_state ^ board.board_state
-    board.board_state = board.board_state | 1 << board.col_height[col]
-    board.other_player_state = board.cur_player_state ^ board.board_state
+    move = 1 << board.col_height[col]
+    board.col_height[col] += 1
+
+    board.player_states[board.cur_player] ^= move
+    #board.board_state ^= board.player_states[board.cur_turn]
+
+    board.moves.append(col)
     board.moves_made+=1
 
-    #swap colours
-    temp_colour = board.cur_player_colour
-    board.cur_player_colour = board.other_player_colour
-    board.other_player_colour = temp_colour
+    board.cur_player = not board.cur_player
+
+def undo_move(board):
+    col = board.moves.pop()
+    board.moves_made-=1
+
+    board.col_height[col] -= 1
+    move = 1 << board.col_height[col]
+
+    board.player_states[not board.cur_player] ^= move
+
+    board.cur_player = not board.cur_player
+    #board.board_state ^= board.player_states[board.cur_turn] #do we need this?
+
 
 def evaluation(board):
+    return score(board.player_states[board.cur_player]) - \
+           score(board.player_states[not board.cur_player])
 
-    return score(board.cur_player_state) - score(board.other_player_state)
-
-
-def score(pos):
+def score(state):
 
     #Note: we subtract double counts when counting how many 2/3 in a rows there are.
     #      eg. in every 4 in a row there is 2 x three in a rows.
  
-    four_in_row = num_in_row(4,pos)
-    three_in_row = num_in_row(3,pos) - 2*four_in_row 
-    two_in_row = num_in_row(2,pos) - 3*four_in_row - 2*three_in_row
-    return pos.bit_count() + 10*two_in_row + 100*three_in_row + 1000*four_in_row
+    four_in_row = num_in_row(4,state)
+    three_in_row = num_in_row(3,state) - 2*four_in_row 
+    two_in_row = num_in_row(2,state) - 3*four_in_row - 2*three_in_row
+    return bin(state).count("1") + 10*two_in_row + 100*three_in_row + 1000*four_in_row
 
-def num_in_row(count, pos):
+def num_in_row(count, state):
     total = 0
     directions = {VERTICAL,RIGHT_DIAG,LEFT_DIAG,HORIZONTAL}
     
@@ -118,10 +133,10 @@ def num_in_row(count, pos):
     #See: https://towardsdatascience.com/creating-the-perfect-connect-four-ai-bot-c165115557b0
     
     for dir in directions:
-        m = pos
+        m = state
         for j in range(1,count):
             m = m & (m >> dir)
-        total += m.bit_count()
+        total += bin(m).count("1")
 
     return total
 
@@ -133,30 +148,66 @@ def print_board(board):
         print('| ', end="")
         for j in range(0,board.width):
             cur = (i + j*(board.height+1))
-
-            if (board.cur_player_state & (1 << cur)):
-                print(board.cur_player_colour + ' ', end='')
-            elif (board.other_player_state & (1 << cur)):
-                print(board.other_player_colour + ' ', end='')
+            if (board.player_states[RED] & (1 << cur)):
+                print('r ', end='')
+            elif (board.player_states[YEL] & (1 << cur)):
+                print('y ', end='')
             else:
                 print('. ', end='')
 
         print('|')
     print('+===============+')
 
+def min_max(board, depth):
+    j,v = max_value(board,depth)
+    print(j,v)
+
+def max_value(board, depth):
+    #check terminal states
+    if num_in_row(4,board.player_states[not board.cur_player]):
+        if board.cur_player == board.our_player:
+            return -1,10000
+        else:
+            return -1,-10000
+    elif depth == 0:
+        return evaluation(board)
+    v = -1000000 #make this more appropriate
+    for j in range(0,board.width):
+        if board.col_height[j] % 7 != 6: #check if col is full
+            make_move(board,j)
+            print("AFTER MOVE")
+            print_board(board)
+            v = max(v,min_value(board, depth-1)[1])
+            print(v)
+            print_board(board)
+    return j,v
+
+def min_value(board, depth):
+    #check terminal states
+    if num_in_row(4,board.player_states[not board.cur_player]):
+        if board.cur_player == board.our_player:
+            return -1,10000
+        else:
+            return -1,-10000
+    elif depth == 0:
+        return -1,evaluation(board)
+    v = -1000000 #make this more appropriate
+    for j in range(0,board.width):
+        if board.col_height[j] % 7 != 6: #check if col is full
+            make_move(board,j)
+            print_board(board)
+            v = max(v,max_value(board, depth-1)[1])
+            undo_move(board)
+    return j,v
+
 def connect_four_mm(contents, turn, max_depth):
 
     board = convert_state_to_player_pos(turn[0],contents)
+    print("STARTING BOARD")
     print_board(board)
-
-    #print(board.col_height)
-    make_move(board, 6)
-    print_board(board)
-    #print(evaluation(board))
-
-    return ''
+    return min_max(board, max_depth)
 
 if __name__ == '__main__':
     # Example function call below, you can add your own to test the connect_four_mm function
     #connect_four_mm(".ryyrry,.rryry.,..y.r..,..y....,.......,.......", "red", 4)
-    connect_four_mm("..yyrrr,..ryryr,....y..,.......,.......,.......", "red", 4)
+    connect_four_mm("..yyrrr,..ryryr,....y..,.......,.......,.......", "red", 1)
