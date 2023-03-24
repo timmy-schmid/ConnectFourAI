34f8e4d168713455897c63c64e10d3abe42296fb
Tim Schmid <tim.a.schmid@gmail.com>
2023-03-24 12:24:59 +1100

    Minimax complete - all test cases passed

--- program.py
+++ program.py
@@ -1,5 +1,9 @@
 #SID: 500687806
 
+from operator import itemgetter
+
+log_enabled = False
+
 VERTICAL = 1
 RIGHT_DIAG = 6
 LEFT_DIAG = 8
@@ -26,7 +30,6 @@ class Board:
     |0  7 14 21 28 35 42|
     +===================+
     '''
-
     def __init__(self):
 
         self.height = 6
@@ -41,6 +44,7 @@ class Board:
         self.player_states = [b'',b'']
         self.board_state = b''
         self.moves = []
+        self.analysed_moves = 0
 
 def convert_state_to_player_pos(turn, contents):
 
@@ -111,19 +115,24 @@ def undo_move(board):
     #board.board_state ^= board.player_states[board.cur_turn] #do we need this?
 
 
-def evaluation(board):
-    return score(board.player_states[board.cur_player]) - \
-           score(board.player_states[not board.cur_player])
+def eval(board, depth, is_maximiser):
+    #check terminal state - if previous player  has made 4 in a row
+    if num_in_row(4,board.player_states[not board.cur_player]):
+        return -10000 if is_maximiser else 10000
+
+    elif depth == 0:
+        return score(board.player_states[board.our_player]) - \
+            score(board.player_states[not board.our_player])
 
 def score(state):
 
     #Note: we subtract double counts when counting how many 2/3 in a rows there are.
     #      eg. in every 4 in a row there is 2 x three in a rows.
  
-    four_in_row = num_in_row(4,state)
-    three_in_row = num_in_row(3,state) - 2*four_in_row 
-    two_in_row = num_in_row(2,state) - 3*four_in_row - 2*three_in_row
-    return bin(state).count("1") + 10*two_in_row + 100*three_in_row + 1000*four_in_row
+    #four_in_row = num_in_row(4,state)
+    three_in_row = num_in_row(3,state) #- 2*four_in_row 
+    two_in_row = num_in_row(2,state) - 2*three_in_row #3*four_in_row 
+    return bin(state).count("1") + 10*two_in_row + 100*three_in_row #+ 1000*four_in_row
 
 def num_in_row(count, state):
     total = 0
@@ -140,74 +149,77 @@ def num_in_row(count, state):
 
     return total
 
-#Useful for debugging states.
-def print_board(board):
-    print("Move " + str(board.moves_made))
-    print('+===============+')
-    for i in range(board.height,-1,-1):
-        print('| ', end="")
-        for j in range(0,board.width):
-            cur = (i + j*(board.height+1))
-            if (board.player_states[RED] & (1 << cur)):
-                print('r ', end='')
-            elif (board.player_states[YEL] & (1 << cur)):
-                print('y ', end='')
-            else:
-                print('. ', end='')
+def minimax(board, depth, is_maximiser):
+    score = eval(board, depth, is_maximiser)
 
-        print('|')
-    print('+===============+')
+    if score is not None:
+        return score
+    
+    scores = []
 
-def min_max(board, depth):
-    j,v = max_value(board,depth)
-    print(j,v)
+    for col in available_cols(board):
+        make_move(board,col)
+        board.analysed_moves += 1
+        scores.append(minimax(board,depth-1,is_maximiser=not is_maximiser))
+        undo_move(board)
+    return (max if is_maximiser else min)(scores)
 
-def max_value(board, depth):
-    #check terminal states
-    if num_in_row(4,board.player_states[not board.cur_player]):
-        if board.cur_player == board.our_player:
-            return -1,10000
-        else:
-            return -1,-10000
-    elif depth == 0:
-        return evaluation(board)
-    v = -1000000 #make this more appropriate
-    for j in range(0,board.width):
-        if board.col_height[j] % 7 != 6: #check if col is full
-            make_move(board,j)
-            print("AFTER MOVE")
-            print_board(board)
-            v = max(v,min_value(board, depth-1)[1])
-            print(v)
-            print_board(board)
-    return j,v
+def find_best_move(board,depth):
+    board.analysed_moves = 1
+    moves = []
+    for col in available_cols(board):
+        make_move(board,col)
+        board.analysed_moves += 1
+        moves.append((minimax(board,depth-1, is_maximiser = False),col))
+        undo_move(board)
+    log_msg("Possible move evals: " + str(moves),log_enabled)
+    return max(moves,key=itemgetter(0))
 
-def min_value(board, depth):
-    #check terminal states
-    if num_in_row(4,board.player_states[not board.cur_player]):
-        if board.cur_player == board.our_player:
-            return -1,10000
-        else:
-            return -1,-10000
-    elif depth == 0:
-        return -1,evaluation(board)
-    v = -1000000 #make this more appropriate
-    for j in range(0,board.width):
-        if board.col_height[j] % 7 != 6: #check if col is full
-            make_move(board,j)
-            print_board(board)
-            v = max(v,max_value(board, depth-1)[1])
-            undo_move(board)
-    return j,v
+#returns the list of columns that are possible to play in
+def available_cols(board):
+    return [j for j in range(0,board.width)
+            if (board.col_height[j] % 7 != 6)]
 
 def connect_four_mm(contents, turn, max_depth):
 
     board = convert_state_to_player_pos(turn[0],contents)
-    print("STARTING BOARD")
-    print_board(board)
-    return min_max(board, max_depth)
+    log_msg("STARTING BOARD",log_enabled)
+    log_msg("We are playing as " + turn,log_enabled)
+    log_board(board,log_enabled)
+    best_move = find_best_move(board,max_depth)
+
+    #print_board(board)
+    log_msg("------\nSummary\n------", log_enabled)
+    log_msg("Running minimax at depth: " + str(max_depth),log_enabled)
+    log_msg("Best Move: " + str((best_move[1])), log_enabled)
+    log_msg("Score: " + str((best_move[0])), log_enabled)
+    log_msg("Moves Analysed: " + str(board.analysed_moves), log_enabled)
+    return str(best_move[1]) + '\n' + str(board.analysed_moves)
+
+def log_msg(msg,enabled):
+    if enabled:
+        print(msg)
+
+def log_board(board, enabled):
+    if enabled:
+        print("Move " + str(board.moves_made))
+        print('+===============+')
+        for i in range(board.height,-1,-1):
+            print('| ', end="")
+            for j in range(0,board.width):
+                cur = (i + j*(board.height+1))
+                if (board.player_states[RED] & (1 << cur)):
+                    print('r ', end='')
+                elif (board.player_states[YEL] & (1 << cur)):
+                    print('y ', end='')
+                else:
+                    print('. ', end='')
+
+            print('|')
+        print('+===============+')
 
 if __name__ == '__main__':
     # Example function call below, you can add your own to test the connect_four_mm function
     #connect_four_mm(".ryyrry,.rryry.,..y.r..,..y....,.......,.......", "red", 4)
-    connect_four_mm("..yyrrr,..ryryr,....y..,.......,.......,.......", "red", 1)
+    #connect_four_mm("..yyrrr,..ryryr,....y..,.......,.......,.......", "red", 1)
+    connect_four_mm("..y.r..,..y.r..,..y.r..,.......,.......,.......", "red", 1)
